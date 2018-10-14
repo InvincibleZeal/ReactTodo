94da409f445a31bb1f3b8fb28b5b9f2e8e1db749
Ritesh Ganjewala <42371840+InvincibleZeal@users.noreply.github.com>
2018-10-14 14:54:55 +0530

    built package (#2)

---
+++ build/precache-manifest.fb1f5d0f6d20e35d3dffbddf2cd40dc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f4c337290654721cc95",
+    "url": "/static/js/main.4f4c3372.chunk.js"
+  },
+  {
+    "revision": "dae8aa457ab1f0aef9e0",
+    "url": "/static/js/1.dae8aa45.chunk.js"
+  },
+  {
+    "revision": "4f4c337290654721cc95",
+    "url": "/static/css/main.3abda966.chunk.css"
+  },
+  {
+    "revision": "67eecbd3d4848c3168a9703c9e965ab3",
+    "url": "/index.html"
+  }
+];
