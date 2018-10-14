5f79a96197ec30af2b741f8288926b5afe62bb0f
InvincibleZeal <ritesh.ganjewala17@gmail.com>
2018-10-14 14:47:01 +0530

    built package

---
+++ build/precache-manifest.452d18504675c0824caa10977b2034c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fed97bd4818d2070f6d4",
+    "url": "/static/js/main.fed97bd4.chunk.js"
+  },
+  {
+    "revision": "dae8aa457ab1f0aef9e0",
+    "url": "/static/js/1.dae8aa45.chunk.js"
+  },
+  {
+    "revision": "fed97bd4818d2070f6d4",
+    "url": "/static/css/main.4364c9f4.chunk.css"
+  },
+  {
+    "revision": "29f783e57dcea4bc45f8f55d357d84a3",
+    "url": "/index.html"
+  }
+];
